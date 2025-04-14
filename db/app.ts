9cdf9d311c4eefa2ed9059c0f28f721a84a70fb5
CharmingBeetle <nina.e.thomas@gmail.com>
2025-04-14 10:34:43 +0100

    feat: added quiz/questions/attempt endpoints

--- db/app.ts
+++ db/app.ts
@@ -13,27 +13,45 @@ const { postUsers} = require("./controllers/users.controller")
 app.post("/users", postUsers)
 
 //PDF files
-app.post("/files")
-app.post("/upload")
+app.post("/files",  )
+app.post("/upload", uploadFiles)
 
 //Quizzes
-app.post("/quizzes") 
-app.get("/quizzes/:user_id")
-app.patch("/quizzes/:quiz_id")
-app.delete("/quizzes/:quid_id") // to be added later
+app.post("/quizzes", postQuizzes) 
+app.get("/quizzes/:user_id", getQuizByUserId)
+app.patch("/quizzes/:quiz_id", updateQuizById)
+app.delete("/quizzes/:quid_id", deleteQuizById) // to be added later
+
+//Quiz Questions
+app.get("/questions/:quiz_id", getQuestionsById)
+app.post("questions", postQuestions )
+
+//Question Options
+app.get("/question_options/:question_id", getOptionsByQuestionId)
+app.post("question_opitons", postOptions)
 
 
+//Attempted answers
+app.get("/attempt_answer/:question_options_id", getAnswerAttemptByOptionId)
+app.post("attempt_answer/:question_options_id", postAnswerAttemptByOptionId)
 
+
+//Quiz attempt
+app.post("/attempt", postQuizAttempt)
+app.patch("/attempt/:attempt_id"), updateQuizAttemptById
 
 const { handleServerErrors, 
     handlePsqlErrors, 
     handleCustomErrors,
 } = require("./controllers/errors.controller")
 
+
+
+
 //ERROR HANDLING
-app.all("*", req, res => {
-    res.status(404).send({msg: "Path not found"})
-})
+app.all("*", (req, res) => {
+    res.status(404).send({ msg: "Path not found" });
+  });
 
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);

0b010468a9aee458297950ef2f1059064e5893fd
joshnajoshy <joshnajoshy@yahoo.co.uk>
2025-04-16 15:26:05 +0100

    feat: updated test data

--- db/data/test-data/questions-data.ts
+++ db/data/test-data/questions-data.ts
@@ -5,7 +5,7 @@ interface QuestionsTestDataItem {
 const questionsTestData: QuestionsTestDataItem[] = [
   { quiz_id: 1, question_text: "What is the chemical symbol for water?" },
   { quiz_id: 1, question_text: "How many planets are in our solar system?" },
-  { quiz_id: 2, question_text: "Who was the first Roman Emperor?" },
-  { quiz_id: 3, question_text: "What is 2 + 2?" },
+  { quiz_id: 1, question_text: "Who was the first Roman Emperor?" },
+  { quiz_id: 1, question_text: "What is 2 + 2?" },
 ];
 export default questionsTestData;
